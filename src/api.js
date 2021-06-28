14748e7a65068ff568a86fa08f01480a05fddbbb
Guilherme Santos <gsousadev@gmail.com>
2021-06-28 11:35:47 -0300

    Finalizando front para teste com git pages

--- src/api.js
+++ src/api.js
@@ -23,6 +23,13 @@ api.interceptors.request.use(
 
 api.interceptors.response.use(
     response => {
+
+        response.header('Access-Control-Allow-Origin','*');
+
+        response.header('Access-Control-Allow-Methods','GET, POST, OPTIONS, PUT, PATCH, DELETE');
+        
+        response.header('Access-Control-Allow-Headers','Origin,Content-Type,X-Requested-With,Accept,Authorization');
+
         return response.data;
     },
     error => {
